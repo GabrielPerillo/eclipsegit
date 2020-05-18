f87ee274205938ee88330969861d96e9b3d4dfbd
Gabriel Perillo <gabriel.perillo2011@gmail.com>
2020-05-18 18:55:29 -0300

    Novo commit pelo eclipse.

--- EclipseGit/src/br/maua/git/HelloGit.java
+++ EclipseGit/src/br/maua/git/HelloGit.java
@@ -5,7 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git...");
 		
-		System.out.println("Hello GitHub - Alterado no GitHub");
+		System.out.println("Hello GitHub - Alterado no GitHub...");
+		
+		System.out.println("Hello Eclipse - Modificado no Eclipse...");
 	}
 
 }
